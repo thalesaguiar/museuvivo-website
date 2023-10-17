17d4e7bbedc8f6e9d07d8059c9ba4d7aa080803e
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-10-17 19:09:08 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>olá mundo</p>
+    </div>
+  );
+}
