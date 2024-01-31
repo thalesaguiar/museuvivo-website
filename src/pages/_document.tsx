05e97cbb18116c5f1949663884e2da1921a28a4b
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-01-30 23:42:21 -0300

    feat: new font added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   render() {
@@ -7,6 +8,10 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.cdnfonts.com/css/helvetica-neue-5"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
