e4d0fad4116a85fb34f4911730a88423a7b3c42d
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-01-31 22:28:26 -0300

    feat: created new component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Header/Header.module.css";
 import Image from "next/image";
 import Logo from "../../../public/logo.svg";
 import MenuSvg from "../../../public/menu.svg";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <Image src={Logo} alt="Logo do Museu" width={50} height={50} />
-      <Image src={MenuSvg} alt="Menu icon" width={30} height={30} />
     </div>
   );
 }
