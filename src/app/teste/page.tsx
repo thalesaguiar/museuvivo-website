b754221d37cef11c8926ace420c9f8f0b4c35e1d
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-02 21:18:51 -0300

    feat: create new component

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/components/NavBar/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <text></text>
+    </div>
+  );
+}
