bdfbf5a337d7f12bc4f36024a255b8a7c4215b81
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-03 22:17:34 -0300

    feat: new styles add

--- src/components/ImagesSlider/ImagesSlider.tsx
+++ src/components/ImagesSlider/ImagesSlider.tsx
@@ -1,39 +1,50 @@
 import Image from "next/image";
 import MuseumPic from "../../../public/museumE.png";
 import styles from "./ImagesSlider.module.css";
-import { Inter } from "next/font/google";
 import FrontPic from "../../../public/museumE.jpg";
+import Button from "../Button";
+
 export default function ImagesSlider() {
   return (
     <div className={styles.container}>
       <div
         style={{
           display: "flex",
+          position: "absolute",
+          flexDirection: "column",
           backgroundColor: "#202020",
           width: "100%",
           height: "100%",
         }}
       />
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "#0505059b",
+          position: "absolute",
+          opacity: 0.1,
+        }}
+      ></div>
       <Image
         src={FrontPic}
         alt="Fachada do Museu Vivo"
         fill
-        style={{ objectFit: "cover" }}
+        style={{ objectFit: "cover", opacity: 0.6 }}
         quality={75}
       />
-
-      <h2
+      <p
         style={{
           color: "#fff",
-          fontSize: "4rem",
+          fontSize: "3rem",
           position: "absolute",
           zIndex: 2,
           textAlign: "center",
-          fontFamily: "Inter",
+          marginBottom: "5rem",
         }}
       >
-        Bem Vindo ao Museu Vivo
-      </h2>
+        ΓNΩθΙ ΣΕΑΥΤΟΝ
+      </p>
+      <Button />
     </div>
   );
 }
