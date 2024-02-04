867c9eee8c9c3e5468120c02c280b66ea5507b6d
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-03 22:16:45 -0300

    feat: create new component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./button.module.css";
+
+export default function Button() {
+  return (
+    <button className={styles.container}>
+      <p>Venha Conhecer</p>
+    </button>
+  );
+}
