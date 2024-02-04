3866edf7def853499f70b62bcd7a4e33921eef78
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-03 22:17:10 -0300

    feat: add google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Junge } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const junge = Junge({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Museu Vivo",
@@ -14,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br" className={junge.className}>
+      <body>{children}</body>
     </html>
   );
 }
