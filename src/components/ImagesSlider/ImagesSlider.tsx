2bfe359e7d42995aa942afb7f07197e064409359
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-04 22:27:59 -0300

    refactor: remove hamburguer menu modal

--- src/components/ImagesSlider/ImagesSlider.tsx
+++ src/components/ImagesSlider/ImagesSlider.tsx
@@ -7,44 +7,21 @@ import Button from "../Button";
 export default function ImagesSlider() {
   return (
     <div className={styles.container}>
-      <div
-        style={{
-          display: "flex",
-          position: "absolute",
-          flexDirection: "column",
-          backgroundColor: "#202020",
-          width: "100%",
-          height: "100%",
-        }}
-      />
-      <div
-        style={{
-          display: "flex",
-          backgroundColor: "#0505059b",
-          position: "absolute",
-          opacity: 0.1,
-        }}
-      ></div>
-      <Image
-        src={FrontPic}
-        alt="Fachada do Museu Vivo"
-        fill
-        style={{ objectFit: "cover", opacity: 0.6 }}
-        quality={75}
-      />
-      <p
-        style={{
-          color: "#fff",
-          fontSize: "3rem",
-          position: "absolute",
-          zIndex: 2,
-          textAlign: "center",
-          marginBottom: "5rem",
-        }}
-      >
-        ΓNΩθΙ ΣΕΑΥΤΟΝ
-      </p>
-      <Button />
+      <div className={styles.imageWrapper}>
+        <Image src={FrontPic} alt="Fachada do Museu Vivo" fill quality={75} />
+      </div>
+      <div className={styles.sectionWrapper}>
+        <p
+          style={{
+            color: "#fff",
+            fontSize: "3rem",
+            textAlign: "center",
+          }}
+        >
+          ΓNΩθΙ ΣΕΑΥΤΟΝ
+        </p>
+        <Button />
+      </div>
     </div>
   );
 }
