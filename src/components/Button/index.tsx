aade01a1a26c9adbd1c5a00a2c5076f785b64822
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-06 21:27:41 -0300

    refactor: change title text

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import styles from "./button.module.css";
 export default function Button() {
   return (
     <button className={styles.container}>
-      <p>Venha Conhecer</p>
+      <p>VENHA CONHECER</p>
     </button>
   );
 }
