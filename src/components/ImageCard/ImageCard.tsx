d29afb48bcc73e31379218e4712426ca51f34ab5
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-09 00:15:17 -0300

    feat: add new media query for image card responsiveness

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -3,39 +3,144 @@ import CardImg1 from "../../../public/1.png";
 import CardImg2 from "../../../public/2.png";
 import CardImg3 from "../../../public/3.png";
 import CardImg4 from "../../../public/4.png";
-
+import CardImg5 from "../../../public/5.png";
+import CardImg6 from "../../../public/6.png";
+import CardImg1T from "../../../public/1 Tablet.png";
+import CardImg2T from "../../../public/2 Tablet.png";
+import CardImg3T from "../../../public/3 Tablet.png";
+import CardImg4T from "../../../public/4 Tablet.png";
+import CardImg5T from "../../../public/5 Tablet.png";
+import CardImg6T from "../../../public/6 Tablet.png";
 import style from "./ImageCard.module.css";
+
 export default function ImageCard() {
   return (
-    <div className={style.slider}>
-      <Image
-        alt="CardImage"
-        src={CardImg1}
-        quality={80}
-        width={280}
-        height={140}
-      />
-      <Image
-        alt="CardImage"
-        src={CardImg2}
-        quality={80}
-        width={280}
-        height={140}
-      />
-      <Image
-        alt="CardImage"
-        src={CardImg3}
-        quality={80}
-        width={280}
-        height={140}
-      />
-      <Image
-        alt="CardImage"
-        src={CardImg4}
-        quality={80}
-        width={280}
-        height={140}
-      />
-    </div>
+    <>
+      <div className={style.sliderMobile}>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg1}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Indíos Botocudos</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg6}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Tropeiros</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg2}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Carvoeiros</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg3}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Estrada de Ferro Vitória Minas</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg4}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Pioneiros e Primeiras Fazendas</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg5}
+            quality={80}
+            width={280}
+            height={140}
+          />
+          <h3>Forja</h3>
+        </div>
+      </div>
+      {/* Desktop */}
+      <div className={style.sliderTablet}>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg1T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Indíos Botocudos</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg6T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Tropeiros</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg2T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Carvoeiros</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg3T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Estrada de Ferro Vitória Minas</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg4T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Pioneiros e Primeiras Fazendas</h3>
+        </div>
+        <div>
+          <Image
+            alt="CardImage"
+            src={CardImg5T}
+            quality={80}
+            width={700}
+            height={380}
+          />
+          <h3>Forja</h3>
+        </div>
+      </div>
+    </>
   );
 }
