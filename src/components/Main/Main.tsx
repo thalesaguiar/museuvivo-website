300f654bbf76e8dbedaa1c7781551cc3c3f408dd
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-10 19:24:35 -0300

    feat: add new component and add styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,29 +1,35 @@
 import ImageCard from "../ImageCard/ImageCard";
+import InfoCard from "../InfoCard/InfoCard";
 import style from "./Main.module.css";
 export default function MainSection() {
   return (
-    <section className={style.container}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingInline: "30px",
-          marginTop: "50px",
-        }}
-      >
-        <p style={{ fontSize: "2rem" }}>
-          Em <br />
-          Exposição
-        </p>
-        <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+    <>
+      <div className={style.container}>
+        <div>
+          <InfoCard />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingInline: "20px",
+            marginTop: "50px",
+          }}
+        >
+          <p style={{ fontSize: "2.5rem", fontWeight: "bold" }}>
+            Em <br />
+            Exposição
+          </p>
+          {/* <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
           Veja todas as exposições
-        </p>
+        </p> */}
+        </div>
+        <div>
+          <ImageCard />
+        </div>
       </div>
-      <div>
-        <ImageCard />
-      </div>
-    </section>
+    </>
   );
 }
