6f6e9082c27e5182a5aac21eff89bf5dedd672de
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-02-21 23:21:28 -0300

    feat: add video component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,19 +5,19 @@ import MuseumPic from "../../../public/museum1x.jpg";
 import MuseumPic2 from "../../../public/museum2x.jpg";
 import styles from "./Header.module.css";
 import Button from "../Button";
+import Script from "next/script";
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.desktopContainer}>
-        <Image
-          src={MuseumPic}
-          alt="Fachada do museu"
-          fill
-          style={{ objectFit: "cover" }}
-          quality={80}
-          priority
-        />
+        <section className={styles.videoWrapper}>
+          <iframe
+            src="https://player.vimeo.com/video/915171477?speed=0&pip=0&loop=1&background=1&app_id=122963"
+            className={styles.iframe}
+          ></iframe>
+        </section>
+        <Script src="https://player.vimeo.com/api/player.js" async></Script>
       </div>
       <div className={styles.mobileContainer}>
         <Image
