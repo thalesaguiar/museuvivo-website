7bdb5e64b5dff5d899f7fa01eb0e993d87e8360d
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-04-08 23:44:00 -0300

    feat: create new page

---
+++ src/app/IndiosBotocudos/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/components/Menu/Menu";
+
+export default function Home() {
+  return (
+    <div>
+      <text>Indios Botocudos</text>
+    </div>
+  );
+}
