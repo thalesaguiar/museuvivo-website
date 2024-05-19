96b574e91461974d3bff1d559dea32050acbc858
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-05-19 16:15:35 -0300

    feat: create new component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,7 @@
+export function Divider() {
+  return (
+    <div className="flex w-full items-center justify-center mt-20 mb-20">
+      <div className="w-11/12 h-0.5 bg-black rounded-xl opacity-20"></div>
+    </div>
+  );
+}
