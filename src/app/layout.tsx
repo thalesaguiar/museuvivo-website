89df2eb643d0c8d57d8d86a91a0a45960ead0e48
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-05-30 14:33:50 -0300

    feat: add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
+"use client";
 import type { Metadata } from "next";
-import { Roboto, Fira_Sans } from "next/font/google";
+import { Roboto, Fira_Sans, DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const roboto = Fira_Sans({
-  weight: "400",
+export const roboto = Fira_Sans({
+  weight: ["400", "300", "200", "700"],
   subsets: ["latin"],
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "Museu Vivo",
-};
+export const dm_Sans = DM_Sans({
+  weight: ["400", "300", "200", "700", "600"],
+  subsets: ["latin"],
+  display: "optional",
+});
+
+// export const metadata: Metadata = {
+//   title: "Museu Vivo",
+// };
 
 export default function RootLayout({
   children,
