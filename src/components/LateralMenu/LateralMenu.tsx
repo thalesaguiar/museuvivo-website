1c22e3fef46f98773fde28e008ca3f8ee56ce67b
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-05-30 14:33:40 -0300

    styles: add new styles

--- src/components/LateralMenu/LateralMenu.tsx
+++ src/components/LateralMenu/LateralMenu.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React from "react";
 import { Divider } from "../Divider/Divider";
-
+import { dm_Sans } from "@/app/layout";
 const route = [
   {
     title: "Inicio",
@@ -28,36 +29,75 @@ const show = true;
 
 export function LateralMenu() {
   return show ? (
-    <div className=" flex fixed right-0 left-100 bottom-0 z-50 bg-neutral-900 w-1/3 h-full">
-      <div className="flex w-full h-full justify-center flex-col p-20">
-        <p className="text-2xl font-semibold opacity-70 text-white ml-5">
-          Menu
+    <div className="flex justify-between fixed right-0 left-100 bottom-0 z-50 bg-neutral-900 w-2/5 max-sm:w-full h-full md:px-32 md:py-20">
+      <div className="flex w-full h-full justify-center flex-col max-sm:p-10">
+        <p
+          className={` ${dm_Sans.className} text-2xl font-semibold opacity-70 text-white ml-5 mt-20 `}
+        >
+          MENU
         </p>
         <Divider bgColor="white" />
         <a
-          className="text-8xl p-5 text-white font-sans hover:motion-safe:animate-pulse"
+          className={`${dm_Sans.className} text-8xl p-5 max-sm:text-6xl text-white font-sans font-medium hover:motion-safe:animate-pulse`}
           href="/"
         >
-          Início
+          Inicio
         </a>
-        <a className="text-8xl p-5 text-white font-sans">Ingresso</a>
         <a
-          className="text-8xl p-5 text-white font-sans hover:motion-safe:animate-pulse"
+          className={`${dm_Sans.className} text-8xl p-5 max-sm:text-6xl text-white font-sans font-medium hover:motion-safe:animate-pulse`}
+          href="/"
+        >
+          Ingresso
+        </a>
+        <a
+          className={`${dm_Sans.className} text-8xl p-5 max-sm:text-6xl text-white font-sans font-medium hover:motion-safe:animate-pulse`}
           href="/Sobre"
         >
           Acervo
         </a>
-        <p className="text-8xl p-5 text-white font-sans">Sobre</p>
-        <div className="flex ml-16 mt-20">
-          <p className="text-2xl font-semibold opacity-70 text-white">
-            Redes Sociais
+        <a
+          className={`${dm_Sans.className} text-8xl p-5 max-sm:text-6xl text-white font-sans font-medium hover:motion-safe:animate-pulse`}
+          href="/"
+        >
+          Sobre
+        </a>
+        <div className="flex w-full items-center justify-center mt-20 mb-10 sm:hidden">
+          <div className={`w-11/12 h-0.5 rounded-xl opacity-20 bg-white`}></div>
+        </div>
+        <div className="flex ml-5">
+          <p className="text-xl font-semibold opacity-70 text-white mt-20 mb-5">
+            REDES SOCIAIS
           </p>
         </div>
-        <div className="flex items-center justify-evenly mt-5">
-          <a className="text-xl font-semibold text-white">Instagram</a>
-          <a className="text-xl font-semibold text-white">Facebook</a>
-          <a className="text-xl font-semibold text-white">Linkedin</a>
-          <a className="text-xl font-semibold text-white">Youtube</a>
+        <div className="flex mt-2 max-sm:ml-5">
+          <a
+            className="text-2xl font-semibold text-white mr-8 hover:text-red-400 max-sm:text-xl"
+            href="https://www.instagram.com/museuvivomg/"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            className="text-2xl font-semibold text-white mr-8 hover:text-indigo-600 max-sm:text-xl"
+            href="https://pt-br.facebook.com/MuseuVivoMG/"
+            target="_blank"
+          >
+            Facebook
+          </a>
+          <a
+            className="text-2xl font-semibold text-white mr-8 hover:text-amber-500 max-sm:text-xl"
+            href="https://www.linkedin.com/company/associa%C3%A7%C3%A3o-cultural-museu-vivo/about/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <a
+            className="text-2xl font-semibold text-white mr-8 hover:text-rose-600 max-sm:text-xl"
+            href="https://www.youtube.com/@museuvivo5120"
+            target="_blank"
+          >
+            Youtube
+          </a>
         </div>
       </div>
     </div>
