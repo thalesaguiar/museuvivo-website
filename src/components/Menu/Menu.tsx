624560c97d79fbedc695fd86d29264b2857b0087
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-06-26 18:15:42 -0300

    style: add new styles

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,8 @@
 "use client";
+import Image from "next/image";
 import React, { useState } from "react";
+import Logo from "../../../public/lgmv.png";
+import { DropDownMenu } from "../Dropdown Menu/DropdownMenu";
 
 const route = [
   {
@@ -28,15 +31,16 @@ export default function Menu() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className=" absolute top-0 right-0 mt-10 mr-10">
-      <button
-        onClick={() => console.log("Menu: Botão menu")}
-        className="flex flex-row lg:hidden"
-      >
+    <div className="absolute top-0 right-0 mt-10 flex w-full justify-between p-4">
+        <DropDownMenu />
+        <div className='flex md:hidden'>
         <div className="bg-white w-2 h-2 rounded-full mt-3 mr-5" />
-        <p className="text-3xl text-white"> Menu</p>
-      </button>
-      <div className="flex-row hidden lg:flex">
+        <p className="text-3xl text-white">Menu</p>
+        </div>
+      <div className="flex max-md:hidden">
+        <Image alt="Logo" src={Logo} width={180} className="ml-12" />
+      </div>
+      <div className="flex-row hidden md:flex">
         <a href="/Sobre" className="text-3xl text-white mr-16 cursor-pointer">
           Sobre
         </a>
