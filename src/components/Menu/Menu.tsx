16629b166e5626807543041526398edefa5afecf
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-06-29 11:24:43 -0300

    refactor: remove menu title

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -11,11 +11,6 @@ const route = [
     Icon: "IconBase",
   },
   {
-    title: "Ingresso",
-    href: "#",
-    Icon: "IconBase",
-  },
-  {
     title: "Acervo",
     href: "#",
     Icon: "IconBase",
@@ -32,25 +27,33 @@ export default function Menu() {
 
   return (
     <div className="absolute top-0 right-0 mt-10 flex w-full justify-between p-4">
-        <DropDownMenu />
-        <div className='flex md:hidden'>
+      <DropDownMenu />
+      <div className="flex md:hidden">
         <div className="bg-white w-2 h-2 rounded-full mt-3 mr-5" />
         <p className="text-3xl text-white">Menu</p>
-        </div>
+      </div>
       <div className="flex max-md:hidden">
         <Image alt="Logo" src={Logo} width={180} className="ml-12" />
       </div>
       <div className="flex-row hidden md:flex">
-      <a href="/" className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer">
+        <a
+          href="/"
+          className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer"
+        >
           Inicio
         </a>
-        <a href="/Sobre" className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer">
-        Sobre
+        <a
+          href="/Sobre"
+          className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer"
+        >
+          Sobre
         </a>
-        <a href="/" className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer">
-        Ingresso</a>
-        <a href="/" className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer">
-        Contato</a>
+        <a
+          href="/"
+          className="text-3xl max-lg:text-2xl text-white md:mr-12 cursor-pointer"
+        >
+          Contato
+        </a>
       </div>
     </div>
   );
