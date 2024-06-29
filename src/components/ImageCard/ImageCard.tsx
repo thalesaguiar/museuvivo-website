19ef394231ceb2b1ec732b25ef12b5cf36f92acf
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-06-29 16:47:13 -0300

    style: add new styles

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -37,7 +37,7 @@ export default function ImageCard() {
   return (
     <div
       ref={scrollRef}
-      className="flex w-full flex-row overflow-x-scroll no-scrollbar mb-20 mt-5 group max-md:pl-5"
+      className="flex w-full flex-row overflow-x-scroll no-scrollbar mb-20 mt-5 group max-md:pl-5 p-8"
     >
       <button
         className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-full absolute w-20 h-20 mt-48 ml-10 flex right-16 items-center justify-center opacity-0 group-hover:opacity-100 max-lg:hidden"
